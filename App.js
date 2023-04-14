843782bd8a4b32a8f7f61211954c9f3fbbb61c87
David-Forastieri <davidforastieri@gmail.com>
2023-04-14 15:20:09 -0300

    Desafio 1/Creando app con Expo CLI

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Coder!</Text>
+      <Text>Desafio Número 1</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#616161',
     alignItems: 'center',
     justifyContent: 'center',
   },
